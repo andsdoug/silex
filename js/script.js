ee784b75afe81c8b1aa1eae935bbfd91c3e9dc1e
andsdoug <ander.douglas@gmail.com>
2021-11-21 19:36:49 -0300

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
-        $(document).ready(function() {
-        	$('.fancybox').each(function () {
-        	    var href = this.getAttribute('data-silex-href') || this.getAttribute('href');
-        	    if (this.getAttribute('data-silex-href')) {
-            	    this.removeAttribute('data-silex-href');
-            	    // for the fancybox script
-            	    this.setAttribute('href', href);
-            	    // prevent silex from opening link
-            	    $(this).off('click');
-        	    }
-        	    // this is a galery
-        	    this.setAttribute('data-fancybox-group', 'group1');
-        	});
-        	// create the fancybox
-        	$('.fancybox').fancybox();
-        });
+    window.silex.data = {"site":{"width":972},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+
+/*
+ * active menu widget for Silex
+ * create an element which links to an anchor, e.g. an element with a link to #anchor1
+ * add the css class "anchor-link" to this element
+ * create an element which is the anchor, e.g. an element with the css class "anchor1"
+ * when the user clicks on the link, the scroll slides until the element is visible
+ * when the user slides and the element is visible, the link gets a css class "active-menu"
+ */
+$(function() {
+    var container = $('.anchor-container');
+    var containerTop = 0;
+    $(window).scroll(checkScroll);
+    checkScroll();
+    function checkScroll(){
+        var fromTop = $(this).scrollTop();
+        if(containerTop < fromTop) {
+           container.addClass('offscreen');
+        }
+        else {
+           container.removeClass('offscreen');
+        }
+    }
+});
