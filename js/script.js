c6ea5b27baec794c033b868c06abaf7a7507f554
andsdoug <ander.douglas@gmail.com>
2022-01-01 16:19:08 -0300

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
-        $(document).ready(function() {
-        	$('.fancybox').each(function () {
-        	    var href = this.getAttribute('data-silex-href') || this.getAttribute('href');
-        	    if (this.getAttribute('data-silex-href')) {
-            	    this.removeAttribute('data-silex-href');
-            	    // for the fancybox script
-            	    this.setAttribute('href', href);
-            	    // prevent silex from opening link
-            	    $(this).off('click');
-        	    }
-        	    // this is a galery
-        	    this.setAttribute('data-fancybox-group', 'group1');
-        	});
-        	// create the fancybox
-        	$('.fancybox').fancybox();
-        });
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
